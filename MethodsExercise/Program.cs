34e7fa34a39ac1aea2a92b03c8b6ac2cdd01fb0e
TrillOnGit <austinfunk99@yahoo.ca>
2023-06-19 11:58:00 -0400

    Wrote a short story for the exercise and some code to confirm it runs

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your name?");
+            var userName = Console.ReadLine();
+            Console.WriteLine("What's your favorite band?");
+            var userBand = Console.ReadLine();
+            Console.WriteLine("What's your favorite Color?");
+            var userColor = Console.ReadLine();
+            Console.WriteLine("What's your favorite animal?");
+            var userAnimal = Console.ReadLine();
+
+            //Code to confirm it runs properly.
+            // Console.WriteLine($"Name: {userName} \nFavorite Color: {userColor}\n" +
+            // $"Favorite Animal: {userAnimal}\nFavorite Band: {userBand}\n");
+            //Short story
+            Console.WriteLine($"Today I saw {userName} riding a {userColor} {userAnimal} while screaming the" +
+                              $"lyrics from {userBand}'s latest song at the top of their longs. Mondays...");
+            
         }
     }
 }
